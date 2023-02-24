ae1240410e109729814d25659c2f77707806a922
vs57github <123260473+vs57github@users.noreply.github.com>
2023-02-24 22:20:22 +0300

    Add files via upload

---
+++ Task64.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число N: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            PrintNumbers(n);          
+        }
+
+        static void PrintNumbers(int n)  // Рекурсивная функция для вывода чисел от N до 1.
+        {
+            if (n == 0) return;
+
+            Console.WriteLine(n);
+
+            PrintNumbers(n - 1);   // Рекурсивный вызов функции. 
+        }
+    }
+}
